0fea66f9910dac8df5c71cffac275557b9ffa2d5
Siddhesh Sawant <siddhesh.sawant1008@gmail.com>
2024-10-13 21:00:01 +0530

    Update dbConfig.jsx

--- src/utils/dbConfig.jsx
+++ src/utils/dbConfig.jsx
@@ -2,6 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-  "postgresql://beat_Cancer_owner:rO6cTMFRd7ix@ep-old-king-a5kd4sn2.us-east-2.aws.neon.tech/cancer?sslmode=require"
+  "ADD UR DRIZZLE ORM URL"
 );
 export const db = drizzle(sql, { schema });
